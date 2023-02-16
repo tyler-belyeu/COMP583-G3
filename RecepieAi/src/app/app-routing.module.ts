098d912273067df204608d18c71bef32303a68a8
StevenFein <saf0929@gmail.com>
2023-02-16 09:18:13 -0800

    Revert ""made minor commit please reject if any errors "" desc.
    This reverts commit 71bbd977d86dd0cfd1ff6f73cd192ac3beb93c0a.

--- RecepieAi/src/app/app-routing.module.ts
+++ RecepieAi/src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { RegisterComponent } from './components/register/register.component';
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LogInComponent },
-  { path: 'register', component: RegisterComponent }
-];
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
