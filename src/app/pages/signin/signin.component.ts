cc5cf8c43e7e6e55e0b29ec887f5bc1d67906e15
StevenFein <saf0929@gmail.com>
2023-04-02 19:06:53 -0700

    Signin form validation

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit{
+  form!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder
+  ){ 
+  }
+  
+ngOnInit(): void {
+  this.form = this.formBuilder.group({
+    email:['',[Validators.required, Validators.email]],
+    password:['',[Validators.required]]
+  });
+}
 }
