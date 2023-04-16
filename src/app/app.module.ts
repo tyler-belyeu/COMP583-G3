e9c47cf8be9c78e805a79cd689a705112e51561b
StevenFein <saf0929@gmail.com>
2023-04-15 17:25:51 -0700

    created ingredients page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import { environment } from '../enviornments/enviornment.defaults';
+import { IngredientsComponent } from './pages/ingredients/ingredients.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/ingredients/ingredients.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredients',
+  templateUrl: './ingredients.component.html',
+  styleUrls: ['./ingredients.component.css']
+})
+export class IngredientsComponent {
+
+  fruits: Array<string> = ['Apple', 'Orange', 'Banana'];
+}
